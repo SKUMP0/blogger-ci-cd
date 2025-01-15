a4e07e88be9a26a8fd7619c81ebe96ed4ecd3d21
SKUMP0 <SKUMP0@users.noreply.github.com>
2025-01-15 02:33:19 +0000

    deploy: b62c4c37447721f7425a95d87276370a6eaf0d31

---
+++ js-instrumented/blog.js
@@ -0,0 +1,794 @@
+"use strict";
+
+function cov_1mpudbopnx() {
+  var path = "C:\\Users\\hamzn\\Documents\\SCHOOL\\Year 2.1\\Sem 2 (PC07)\\DVOPS\\DVOPS_Projects\\Blogger\\Blogger\\public\\js\\blog.js";
+  var hash = "b01458561390ef9a9f4e4bd30cca71449d7de944";
+  var global = new Function("return this")();
+  var gcv = "__coverage__";
+  var coverageData = {
+    path: "C:\\Users\\hamzn\\Documents\\SCHOOL\\Year 2.1\\Sem 2 (PC07)\\DVOPS\\DVOPS_Projects\\Blogger\\Blogger\\public\\js\\blog.js",
+    statementMap: {
+      "0": {
+        start: {
+          line: 2,
+          column: 19
+        },
+        end: {
+          line: 2,
+          column: 21
+        }
+      },
+      "1": {
+        start: {
+          line: 4,
+          column: 19
+        },
+        end: {
+          line: 4,
+          column: 31
+        }
+      },
+      "2": {
+        start: {
+          line: 5,
+          column: 4
+        },
+        end: {
+          line: 5,
+          column: 60
+        }
+      },
+      "3": {
+        start: {
+          line: 6,
+          column: 4
+        },
+        end: {
+          line: 6,
+          column: 64
+        }
+      },
+      "4": {
+        start: {
+          line: 7,
+          column: 4
+        },
+        end: {
+          line: 7,
+          column: 62
+        }
+      },
+      "5": {
+        start: {
+          line: 9,
+          column: 4
+        },
+        end: {
+          line: 13,
+          column: 5
+        }
+      },
+      "6": {
+        start: {
+          line: 10,
+          column: 8
+        },
+        end: {
+          line: 10,
+          column: 82
+        }
+      },
+      "7": {
+        start: {
+          line: 11,
+          column: 8
+        },
+        end: {
+          line: 11,
+          column: 80
+        }
+      },
+      "8": {
+        start: {
+          line: 12,
+          column: 8
+        },
+        end: {
+          line: 12,
+          column: 15
+        }
+      },
+      "9": {
+        start: {
+          line: 15,
+          column: 18
+        },
+        end: {
+          line: 15,
+          column: 38
+        }
+      },
+      "10": {
+        start: {
+          line: 17,
+          column: 4
+        },
+        end: {
+          line: 17,
+          column: 44
+        }
+      },
+      "11": {
+        start: {
+          line: 18,
+          column: 4
+        },
+        end: {
+          line: 18,
+          column: 65
+        }
+      },
+      "12": {
+        start: {
+          line: 20,
+          column: 4
+        },
+        end: {
+          line: 40,
+          column: 6
+        }
+      },
+      "13": {
+        start: {
+          line: 21,
+          column: 8
+        },
+        end: {
+          line: 21,
+          column: 52
+        }
+      },
+      "14": {
+        start: {
+          line: 22,
+          column: 8
+        },
+        end: {
+          line: 22,
+          column: 29
+        }
+      },
+      "15": {
+        start: {
+          line: 23,
+          column: 8
+        },
+        end: {
+          line: 39,
+          column: 9
+        }
+      },
+      "16": {
+        start: {
+          line: 24,
+          column: 12
+        },
+        end: {
+          line: 24,
+          column: 98
+        }
+      },
+      "17": {
+        start: {
+          line: 25,
+          column: 12
+        },
+        end: {
+          line: 25,
+          column: 85
+        }
+      },
+      "18": {
+        start: {
+          line: 27,
+          column: 12
+        },
+        end: {
+          line: 27,
+          column: 56
+        }
+      },
+      "19": {
+        start: {
+          line: 28,
+          column: 12
+        },
+        end: {
+          line: 28,
+          column: 58
+        }
+      },
+      "20": {
+        start: {
+          line: 29,
+          column: 12
+        },
+        end: {
+          line: 29,
+          column: 57
+        }
+      },
+      "21": {
+        start: {
+          line: 32,
+          column: 12
+        },
+        end: {
+          line: 34,
+          column: 13
+        }
+      },
+      "22": {
+        start: {
+          line: 33,
+          column: 16
+        },
+        end: {
+          line: 33,
+          column: 52
+        }
+      },
+      "23": {
+        start: {
+          line: 37,
+          column: 12
+        },
+        end: {
+          line: 37,
+          column: 82
+        }
+      },
+      "24": {
+        start: {
+          line: 38,
+          column: 12
+        },
+        end: {
+          line: 38,
+          column: 84
+        }
+      },
+      "25": {
+        start: {
+          line: 42,
+          column: 4
+        },
+        end: {
+          line: 42,
+          column: 43
+        }
+      },
+      "26": {
+        start: {
+          line: 46,
+          column: 19
+        },
+        end: {
+          line: 46,
+          column: 21
+        }
+      },
+      "27": {
+        start: {
+          line: 47,
+          column: 18
+        },
+        end: {
+          line: 47,
+          column: 38
+        }
+      },
+      "28": {
+        start: {
+          line: 49,
+          column: 4
+        },
+        end: {
+          line: 49,
+          column: 49
+        }
+      },
+      "29": {
+        start: {
+          line: 50,
+          column: 4
+        },
+        end: {
+          line: 50,
+          column: 65
+        }
+      },
+      "30": {
+        start: {
+          line: 52,
+          column: 4
+        },
+        end: {
+          line: 67,
+          column: 6
+        }
+      },
+      "31": {
+        start: {
+          line: 53,
+          column: 8
+        },
+        end: {
+          line: 53,
+          column: 52
+        }
+      },
+      "32": {
+        start: {
+          line: 55,
+          column: 19
+        },
+        end: {
+          line: 55,
+          column: 21
+        }
+      },
+      "33": {
+        start: {
+          line: 56,
+          column: 8
+        },
+        end: {
+          line: 64,
+          column: 9
+        }
+      },
+      "34": {
+        start: {
+          line: 56,
+          column: 21
+        },
+        end: {
+          line: 56,
+          column: 22
+        }
+      },
+      "35": {
+        start: {
+          line: 58,
+          column: 12
+        },
+        end: {
+          line: 63,
+          column: 19
+        }
+      },
+      "36": {
+        start: {
+          line: 66,
+          column: 8
+        },
+        end: {
+          line: 66,
+          column: 65
+        }
+      },
+      "37": {
+        start: {
+          line: 69,
+          column: 4
+        },
+        end: {
+          line: 69,
+          column: 19
+        }
+      }
+    },
+    fnMap: {
+      "0": {
+        name: "addBlog",
+        decl: {
+          start: {
+            line: 1,
+            column: 9
+          },
+          end: {
+            line: 1,
+            column: 16
+          }
+        },
+        loc: {
+          start: {
+            line: 1,
+            column: 19
+          },
+          end: {
+            line: 43,
+            column: 1
+          }
+        },
+        line: 1
+      },
+      "1": {
+        name: "(anonymous_1)",
+        decl: {
+          start: {
+            line: 20,
+            column: 21
+          },
+          end: {
+            line: 20,
+            column: 22
+          }
+        },
+        loc: {
+          start: {
+            line: 20,
+            column: 33
+          },
+          end: {
+            line: 40,
+            column: 5
+          }
+        },
+        line: 20
+      },
+      "2": {
+        name: "viewBlogs",
+        decl: {
+          start: {
+            line: 45,
+            column: 9
+          },
+          end: {
+            line: 45,
+            column: 18
+          }
+        },
+        loc: {
+          start: {
+            line: 45,
+            column: 21
+          },
+          end: {
+            line: 70,
+            column: 1
+          }
+        },
+        line: 45
+      },
+      "3": {
+        name: "(anonymous_3)",
+        decl: {
+          start: {
+            line: 52,
+            column: 21
+          },
+          end: {
+            line: 52,
+            column: 22
+          }
+        },
+        loc: {
+          start: {
+            line: 52,
+            column: 33
+          },
+          end: {
+            line: 67,
+            column: 5
+          }
+        },
+        line: 52
+      }
+    },
+    branchMap: {
+      "0": {
+        loc: {
+          start: {
+            line: 9,
+            column: 4
+          },
+          end: {
+            line: 13,
+            column: 5
+          }
+        },
+        type: "if",
+        locations: [{
+          start: {
+            line: 9,
+            column: 4
+          },
+          end: {
+            line: 13,
+            column: 5
+          }
+        }, {
+          start: {
+            line: undefined,
+            column: undefined
+          },
+          end: {
+            line: undefined,
+            column: undefined
+          }
+        }],
+        line: 9
+      },
+      "1": {
+        loc: {
+          start: {
+            line: 9,
+            column: 8
+          },
+          end: {
+            line: 9,
+            column: 79
+          }
+        },
+        type: "binary-expr",
+        locations: [{
+          start: {
+            line: 9,
+            column: 8
+          },
+          end: {
+            line: 9,
+            column: 28
+          }
+        }, {
+          start: {
+            line: 9,
+            column: 32
+          },
+          end: {
+            line: 9,
+            column: 54
+          }
+        }, {
+          start: {
+            line: 9,
+            column: 58
+          },
+          end: {
+            line: 9,
+            column: 79
+          }
+        }],
+        line: 9
+      },
+      "2": {
+        loc: {
+          start: {
+            line: 23,
+            column: 8
+          },
+          end: {
+            line: 39,
+            column: 9
+          }
+        },
+        type: "if",
+        locations: [{
+          start: {
+            line: 23,
+            column: 8
+          },
+          end: {
+            line: 39,
+            column: 9
+          }
+        }, {
+          start: {
+            line: 36,
+            column: 13
+          },
+          end: {
+            line: 39,
+            column: 9
+          }
+        }],
+        line: 23
+      },
+      "3": {
+        loc: {
+          start: {
+            line: 32,
+            column: 12
+          },
+          end: {
+            line: 34,
+            column: 13
+          }
+        },
+        type: "if",
+        locations: [{
+          start: {
+            line: 32,
+            column: 12
+          },
+          end: {
+            line: 34,
+            column: 13
+          }
+        }, {
+          start: {
+            line: undefined,
+            column: undefined
+          },
+          end: {
+            line: undefined,
+            column: undefined
+          }
+        }],
+        line: 32
+      }
+    },
+    s: {
+      "0": 0,
+      "1": 0,
+      "2": 0,
+      "3": 0,
+      "4": 0,
+      "5": 0,
+      "6": 0,
+      "7": 0,
+      "8": 0,
+      "9": 0,
+      "10": 0,
+      "11": 0,
+      "12": 0,
+      "13": 0,
+      "14": 0,
+      "15": 0,
+      "16": 0,
+      "17": 0,
+      "18": 0,
+      "19": 0,
+      "20": 0,
+      "21": 0,
+      "22": 0,
+      "23": 0,
+      "24": 0,
+      "25": 0,
+      "26": 0,
+      "27": 0,
+      "28": 0,
+      "29": 0,
+      "30": 0,
+      "31": 0,
+      "32": 0,
+      "33": 0,
+      "34": 0,
+      "35": 0,
+      "36": 0,
+      "37": 0
+    },
+    f: {
+      "0": 0,
+      "1": 0,
+      "2": 0,
+      "3": 0
+    },
+    b: {
+      "0": [0, 0],
+      "1": [0, 0, 0],
+      "2": [0, 0],
+      "3": [0, 0]
+    },
+    _coverageSchema: "1a1c01bbd47fc00a2c39e90264f33305004495a9",
+    hash: "b01458561390ef9a9f4e4bd30cca71449d7de944"
+  };
+  var coverage = global[gcv] || (global[gcv] = {});
+  if (!coverage[path] || coverage[path].hash !== hash) {
+    coverage[path] = coverageData;
+  }
+  var actualCoverage = coverage[path];
+  {
+    // @ts-ignore
+    cov_1mpudbopnx = function () {
+      return actualCoverage;
+    };
+  }
+  return actualCoverage;
+}
+cov_1mpudbopnx();
+function addBlog() {
+  cov_1mpudbopnx().f[0]++;
+  var response = (cov_1mpudbopnx().s[0]++, "");
+  var jsonData = (cov_1mpudbopnx().s[1]++, new Object());
+  cov_1mpudbopnx().s[2]++;
+  jsonData.title = document.getElementById("title").value;
+  cov_1mpudbopnx().s[3]++;
+  jsonData.content = document.getElementById("content").value;
+  cov_1mpudbopnx().s[4]++;
+  jsonData.author = document.getElementById("author").value;
+  cov_1mpudbopnx().s[5]++;
+  if ((cov_1mpudbopnx().b[1][0]++, jsonData.title == "") || (cov_1mpudbopnx().b[1][1]++, jsonData.content == "") || (cov_1mpudbopnx().b[1][2]++, jsonData.author == "")) {
+    cov_1mpudbopnx().b[0][0]++;
+    cov_1mpudbopnx().s[6]++;
+    document.getElementById("message").innerHTML = 'All fields are required!';
+    cov_1mpudbopnx().s[7]++;
+    document.getElementById("message").setAttribute("class", "text-danger");
+    cov_1mpudbopnx().s[8]++;
+    return;
+  } else {
+    cov_1mpudbopnx().b[0][1]++;
+  }
+  var request = (cov_1mpudbopnx().s[9]++, new XMLHttpRequest());
+  cov_1mpudbopnx().s[10]++;
+  request.open("POST", "/add-blog", true);
+  cov_1mpudbopnx().s[11]++;
+  request.setRequestHeader('Content-Type', 'application/json');
+  cov_1mpudbopnx().s[12]++;
+  request.onload = function () {
+    cov_1mpudbopnx().f[1]++;
+    cov_1mpudbopnx().s[13]++;
+    response = JSON.parse(request.responseText);
+    cov_1mpudbopnx().s[14]++;
+    console.log(response);
+    cov_1mpudbopnx().s[15]++;
+    if (response.message == undefined) {
+      cov_1mpudbopnx().b[2][0]++;
+      cov_1mpudbopnx().s[16]++;
+      document.getElementById("message").innerHTML = 'Blog: ' + jsonData.title + ' posted!';
+      cov_1mpudbopnx().s[17]++;
+      document.getElementById("message").setAttribute("class", "text-success");
+      cov_1mpudbopnx().s[18]++;
+      document.getElementById("title").value = "";
+      cov_1mpudbopnx().s[19]++;
+      document.getElementById("content").value = "";
+      cov_1mpudbopnx().s[20]++;
+      document.getElementById("author").value = "";
+
+      // Skip redirect during Cypress tests
+      cov_1mpudbopnx().s[21]++;
+      if (!window.Cypress) {
+        cov_1mpudbopnx().b[3][0]++;
+        cov_1mpudbopnx().s[22]++;
+        window.location.href = 'index.html';
+      } else {
+        cov_1mpudbopnx().b[3][1]++;
+      }
+    } else {
+      cov_1mpudbopnx().b[2][1]++;
+      cov_1mpudbopnx().s[23]++;
+      document.getElementById("message").innerHTML = 'Unable to post Blog!';
+      cov_1mpudbopnx().s[24]++;
+      document.getElementById("message").setAttribute("class", "text-danger");
+    }
+  };
+  cov_1mpudbopnx().s[25]++;
+  request.send(JSON.stringify(jsonData));
+}
+function viewBlogs() {
+  cov_1mpudbopnx().f[2]++;
+  var response = (cov_1mpudbopnx().s[26]++, '');
+  var request = (cov_1mpudbopnx().s[27]++, new XMLHttpRequest());
+  cov_1mpudbopnx().s[28]++;
+  request.open('GET', '/view-resources', true);
+  cov_1mpudbopnx().s[29]++;
+  request.setRequestHeader('Content-Type', 'application/json');
+  cov_1mpudbopnx().s[30]++;
+  request.onload = function () {
+    cov_1mpudbopnx().f[3]++;
+    cov_1mpudbopnx().s[31]++;
+    response = JSON.parse(request.responseText);
+    var html = (cov_1mpudbopnx().s[32]++, '');
+    cov_1mpudbopnx().s[33]++;
+    for (var i = (cov_1mpudbopnx().s[34]++, 0); i < response.length; i++) {
+      cov_1mpudbopnx().s[35]++;
+      html += '<tr>' + '<td>' + (i + 1) + '</td>' + '<td>' + response[i].title + '</td>' + '<td>' + response[i].content + '</td>' + '<td>' + response[i].author + '</td>' + '</tr>';
+    }
+    cov_1mpudbopnx().s[36]++;
+    document.getElementById('tableContent').innerHTML = html;
+  };
+  cov_1mpudbopnx().s[37]++;
+  request.send();
+}
